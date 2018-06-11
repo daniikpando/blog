6bd75d7c9ca10ea2edf74436add185195207c273
daniikpando <daniikpando13@hotmail.com>
2018-06-10 20:38:50 -0500

    Creacion de inicio de sesion del usuario

---
+++ src/java/controllers/ServletIniciarSesion.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import dao.UserDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Usuario;
+import utils.Dispatcher;
+
+/**
+ *
+ * @author daniel
+ */
+@WebServlet(name = "ServletIniciarSesion", urlPatterns = {"/login"})
+public class ServletIniciarSesion extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession sesion = request.getSession();
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        
+        UserDAO userdao = new UserDAO();
+        
+        Usuario user = userdao.getUserByEmailAndPass(email, pass);
+        System.out.println(user);
+        
+        if(user != null){
+            sesion.setAttribute("usuario", user);
+        }
+        
+        Dispatcher.irAPagina(request, response, getServletContext(), "/categorias");             
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/login.jsp
+++ web/login.jsp
@@ -9,5 +9,20 @@
     </head>
     <body>
         <h1>Iniciar sesion</h1>
+        <form action="login" method="POST">
+            <div>
+                <label>
+                    Email: <input type="email" placeholder="ingresa tu correo" name="email"> 
+                </label>
+            </div>
+            <div>
+                <label>
+                    Contraseña: <input type="password" placeholder="ingresa tu contraseña" name="pass"> 
+                </label>
+            </div>
+            <div>
+                <input type="submit" value="Enviar" />
+            </div>
+        </form>
     </body>
 </html>
